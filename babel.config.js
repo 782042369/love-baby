3199e790783a8bb5af87d54bed022e0168c416d3
yang <782042369@qq.com>
2018-10-10 14:23:44 +0800

    增加注释

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,14 @@
 module.exports = {
-  presets: ['@vue/app'],
-  plugins: [
-    [
-      'import',
-      { libraryName: 'vant', libraryDirectory: 'es', style: true },
-      'vant'
-    ]
-  ]
+  presets: ['@vue/app']
 }
+// 按需加载 vant 设置
+// module.exports = {
+//   presets: ['@vue/app'],
+//   plugins: [
+//     [
+//       'import',
+//       { libraryName: 'vant', libraryDirectory: 'es', style: true },
+//       'vant'
+//     ]
+//   ]
+// }
